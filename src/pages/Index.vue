24ae25fa9531646bb87ea593a0ab41101f8f2d4d
Ricardo Morin <morinricardo@gmail.com>
2022-05-18 22:23:21 -0700

    Intro content

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,7 +7,7 @@
     </template>
     <section class="section has-text-centered">
       <br>
-      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+      {{ $page.homeContent.intro[$i18n.locale.substr(0, 2)] }}
     </section>
     <section class="section card-section">
       <div class="tile is-ancestor">
@@ -103,6 +103,10 @@
 <page-query>
   query  {
     homeContent (id: "home") {
+      intro {
+        en
+        es
+      }
       cards {
         title {
           en
