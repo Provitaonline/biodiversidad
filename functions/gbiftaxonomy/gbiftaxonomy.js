97336b96d94cbe8db0c78bb3b2022c8c0b3f03a4
Ricardo Morin <morinricardo@gmail.com>
2022-06-01 20:54:25 -0700

    Add proxy to deal with CORS in unpublished GBIF API

---
+++ functions/gbiftaxonomy/gbiftaxonomy.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch')
+exports.handler = async function(event, context) {
+  try {
+    //console.log(event.queryStringParameters.toString())
+    let qry = event.queryStringParameters
+
+    const response = await fetch('https://www.gbif.org/api/occurrence/breakdown' + '?' +  Object.keys(qry).map(key => `${key}=${qry[key]}`).join('&'), {
+      headers: { Accept: 'application/json' },
+    })
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: response.status, body: response.statusText }
+    }
+    const data = await response.json()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    }
+  } catch (err) {
+    console.log(err) // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }), // Could be a custom message or object i.e. JSON.stringify(err)
+    }
+  }
+}
