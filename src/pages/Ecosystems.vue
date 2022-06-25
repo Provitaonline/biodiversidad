f611891a347d96410ab03637675b4d3294ec1408
Ricardo Morin <morinricardo@gmail.com>
2022-06-24 20:44:30 -0700

    Add ecosystems page

---
+++ src/pages/Ecosystems.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout>
+    <template slot="banner">
+      <h1 class="title is-uppercase has-text-centered">
+        {{ $t('label.ecosystems') }}
+      </h1>
+    </template>
+    <section class="section has-text-centered">
+      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$t('label.ecosystems'),
+      titleTemplate: '%s - ' + this.$t('label.site')
+    }
+  },
+}
+</script>
