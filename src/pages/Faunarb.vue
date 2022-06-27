e77ab1f7e89cb2a3ee898f8153e260ecdafbc004
Ricardo Morin <morinricardo@gmail.com>
2022-06-26 20:27:07 -0700

    Faunarb page

---
+++ src/pages/Faunarb.vue
@@ -0,0 +1,29 @@
+<template>
+  <Layout>
+    <template slot="banner">
+      <h1 class="title is-uppercase has-text-centered">
+        {{ $t('label.faunarb') }}
+      </h1>
+    </template>
+    <section class="section has-text-centered">
+      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+    </section>
+  </Layout>
+</template>
+
+<script>
+const taxonomy = require('/content/faunarb/taxonomy.json')
+
+export default {
+  metaInfo() {
+    return {
+      title: this.$t('label.faunarb'),
+      titleTemplate: '%s - ' + this.$t('label.site')
+    }
+  },
+  mounted() {
+    console.log(taxonomy)
+
+  }
+}
+</script>
