35e9d70b18b6f952531d73d17eb1f48239c0cc80
Ricardo Morin <morinricardo@gmail.com>
2022-10-29 19:08:14 -0700

    About styling

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -5,23 +5,33 @@
     </template>
     <section class="content section container">
       <div class="columns">
-        <div class="column" v-html="$page.aboutContent.provita[$i18n.locale.substr(0, 2)]"></div>
+        <div class="column about-block" v-html="$page.aboutContent.provita[$i18n.locale.substr(0, 2)]"></div>
       </div>
       <div class="columns">
-        <div class="column" v-html="$page.aboutContent.mission[$i18n.locale.substr(0, 2)]"></div>
-        <div class="column" v-html="$page.aboutContent.values[$i18n.locale.substr(0, 2)]"></div>
+        <div class="column about-block" v-html="$page.aboutContent.mission[$i18n.locale.substr(0, 2)]"></div>
+        <div class="column about-block" v-html="$page.aboutContent.values[$i18n.locale.substr(0, 2)]"></div>
       </div>
-      <div class="columns">
+      <div class="columns about-block">
         <div class="column" v-html="$page.aboutContent.terms[$i18n.locale.substr(0, 2)]"></div>
       </div>
       <div class="columns">
-        <div class="column" v-html="$page.aboutContent.contact[$i18n.locale.substr(0, 2)]"></div>
-        <div class="column" v-html="$page.aboutContent.address1[$i18n.locale.substr(0, 2)]"></div>
-        <div class="column" v-html="$page.aboutContent.address2[$i18n.locale.substr(0, 2)]"></div>
+        <div class="column about-block" v-html="$page.aboutContent.contact[$i18n.locale.substr(0, 2)]"></div>
+        <div class="column about-block" v-html="$page.aboutContent.address1[$i18n.locale.substr(0, 2)]"></div>
+        <div class="column about-block" v-html="$page.aboutContent.address2[$i18n.locale.substr(0, 2)]"></div>
       </div>
     </section>
   </Layout>
 </template>
+
+<style lang="scss" scoped>
+  @import "~/assets/style/_variables";
+
+  .about-block {
+    margin-bottom: 1em;
+    border-radius: 0.25rem;
+    box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1), 0 0px 0 1px rgba(10, 10, 10, 0.02);
+  }
+</style>
 
 <page-query>
   query {
