4ad8d96c07bb71c612fd6695b9406000c9ece585
Ricardo Morin <morinricardo@gmail.com>
2022-12-05 16:51:38 -0800

    Enable screen view

--- gridsome.client.js
+++ gridsome.client.js
@@ -3,7 +3,8 @@ import VueGtag from 'vue-gtag'
 export default function (Vue, { isServer: disabled, router }) {
   if(process.isClient) {
     Vue.use(VueGtag, {
-      config: { id: 'G-E07PJGQ7ZQ' }
+      config: { id: 'G-E07PJGQ7ZQ' },
+      pageTrackerScreenviewEnabled: true
     }, router)
   }
 }
